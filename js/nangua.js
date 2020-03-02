360f5293eed08409a75c36938e444cc90772ce48
wf021325 <58904011+wf021325@users.noreply.github.com>
2020-03-02 20:02:47 +0800

    Update nangua.js

--- js/nangua.js
+++ js/nangua.js
@@ -4,7 +4,7 @@ hostname = p.doras.api.vcinema.cn
 ^https\:\/\/p\.doras\.api\.vcinema\.cn\/v5\.0\/user\/\d+$
 */
 let obj = JSON.parse($response.body);
-obj.content.user_id = 1088707;//2020-03-01
+obj.content.user_id = 1000772;//2020-06-02
 //obj.content.user_vip_end_date = "2088.08.08";
 //obj.content.user_vip_state = "2";
 $done({body: JSON.stringify(obj)});
