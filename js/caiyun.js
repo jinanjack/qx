56716671ba665270ffa75376ba7398be2e966490
wf021325 <58904011+wf021325@users.noreply.github.com>
2023-03-20 17:18:01 +0800

    Update caiyun.js

--- js/caiyun.js
+++ js/caiyun.js
@@ -1,6 +1,6 @@
 let obj=JSON.parse($response.body)
 obj.result['xy_svip_expire'] = 4096483190
-obj.result['svip_expired_at']=4096483190
+obj.result['svip_expired_at'] = 4096483190
 
 obj.result.wt.vip['svip_expired_at'] = 4096483190
 
