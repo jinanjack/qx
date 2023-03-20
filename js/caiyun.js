b6a39c106ca1602dcc299a7d6811835b55c8c6f2
wf021325 <58904011+wf021325@users.noreply.github.com>
2023-03-20 17:49:05 +0800

    Update caiyun.js

--- js/caiyun.js
+++ js/caiyun.js
@@ -1,18 +1,13 @@
 let obj=JSON.parse($response.body)
 
-obj.result.svip_expired_at = 4096483190
-obj.result.xy_svip_expire = 4096483190
-obj.result.wt.vip.svip_expired_at = 4096483190
-obj.result.is_vip = true
-obj.result.vip_type ='s'
+obj.result['svip_expired_at'] = 4096483190
+obj.result['xy_svip_expire'] = 4096483190
+obj.result.wt.vip['svip_expired_at'] = 4096483190
+obj.result['is_vip'] = true
+obj.result['vip_type'] ='s'
 
-# obj.result.svip_given = 1
-# 默认0
-
-# obj.result.svip_take_effect = 1
-# SVIP生效
-
-# obj.result.wt.cheat = false
-# 是否为黑户
+obj.result['svip_given'] = 1  //默认0
+obj.result['svip_take_effect'] = 1  //SVIP生效
+obj.result.wt.cheat = false  //是否为黑户
 
 $done({body:JSON.stringify(obj)})
