54664bd5ded49eac426c0119266541487ac7f7ee
wf021325 <58904011+wf021325@users.noreply.github.com>
2023-03-20 17:17:43 +0800

    Update caiyun.js

--- js/caiyun.js
+++ js/caiyun.js
@@ -1,10 +1,12 @@
 let obj=JSON.parse($response.body)
-# obj.result["xy_svip_expire"]=4096483190.9612898827
+obj.result['xy_svip_expire'] = 4096483190
+obj.result['svip_expired_at']=4096483190
 
 obj.result.wt.vip['svip_expired_at'] = 4096483190
+
 obj.result['is_vip'] = true
+obj.result['is_xy_vip'] = true
 obj.result['vip_type'] ='s'
 # obj.result.wt.cheat = false
-
 
 $done({body:JSON.stringify(obj)})
