0c08fc9e66ae39a0bb3f8dce31dedbc2f867612c
wf021325 <58904011+wf021325@users.noreply.github.com>
2023-05-14 22:07:52 +0800

    Update yjb.js

--- js/yjb.js
+++ js/yjb.js
@@ -6,6 +6,7 @@ const wx_account = "/wxapi/account";//用户信息
 
 const rank1 = "/fund_buy_ranking";//加仓榜单
 const rank2 = "/wxapi/fund_holdup_ranking";//飙升榜
+const gz_source = "/fund_gz_source";//切换源
 
 if (url.indexOf(account) != -1||url.indexOf(vip_info) != -1||url.indexOf(wx_account) != -1){
   let obj = JSON.parse(body);
@@ -16,13 +17,17 @@ if (url.indexOf(account) != -1||url.indexOf(vip_info) != -1||url.indexOf(wx_acco
   body = JSON.stringify(obj);
 }
 
-
 if (url.indexOf(rank1) != -1||url.indexOf(rank2) != -1) {
     let obj = JSON.parse(body);
         obj.data.vip_label = true;
     body = JSON.stringify(obj);
 }
 
-
+if (url.indexOf(gz_source) != -1) {
+    //let obj = JSON.parse(body);
+        //obj.data.vip_label = true;
+    //body = JSON.stringify(obj);
+    body = '{"code":200,"message":"SUCCESS","timestamp":1660536270,"data":{"source":2,"list":[1,2,3]}}'
+}
 
 $done({body});
