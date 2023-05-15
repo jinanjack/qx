af3131ba2923607caf8ff6e4a09c259aa698ff8b
wf021325 <58904011+wf021325@users.noreply.github.com>
2023-05-15 20:17:27 +0800

    Update caiyun.js

--- js/caiyun.js
+++ js/caiyun.js
@@ -1,3 +1,16 @@
+/*
+hostname = biz.caiyunapp.com , biz.cyapi.cn ,
+# 旧版   biz.caiyunapp.com
+# 新版   biz.cyapi.cn
+
+# https://biz.cyapi.cn/v2/user?app_name=weather
+# https://biz.cyapi.cn/v1/visitors
+
+#正则匹配^https:\/\/biz\.(caiyunapp\.com|cyapi\.cn)\/v2\/user\?app_name=weather$
+
+^https:\/\/biz.(caiyunapp\.com|cyapi\.cn)\/v(2\/user\?app_name=weather|1/visitors)$ url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+*/
+
 let obj=JSON.parse($response.body)
     //obj.result.svip_expired_at = 3742732800;
     //obj.result.is_xy_vip = true;
