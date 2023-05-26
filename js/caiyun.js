4732e2acdf8a5fa9f396fb14819b01e2f5be93b0
DELLL-5577\huihui <wangfei021325@gmail.com>
2023-05-26 18:26:53 +0800

    no message

--- js/caiyun.js
+++ js/caiyun.js
@@ -6,17 +6,34 @@ hostname = biz.caiyunapp.com , biz.cyapi.cn ,
 # https://biz.cyapi.cn/v2/user?app_name=weather
 # https://biz.cyapi.cn/v1/visitors
 
-# 正则匹配^https:\/\/biz\.(caiyunapp\.com|cyapi\.cn)\/v2\/user\?app_name=weather$
+# https://biz.cyapi.cn/v3/login_by_code 登录
 
-^https:\/\/biz.(caiyunapp\.com|cyapi\.cn)\/v(2\/user\?app_name=weather|1/visitors)$ url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+
+
+# 正则匹配 ^https:\/\/biz.(caiyunapp\.com|cyapi\.cn)\/v(2\/user\?app_name=weather|1/visitors|3\/login_by_code)$
+
+^https:\/\/biz.(caiyunapp\.com|cyapi\.cn)\/v(2\/user\?app_name=weather|1\/visitors)$ url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
 */
 
-let obj=JSON.parse($response.body)
+
+
+let obj = JSON.parse($response.body)
+
+let Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY'
+
+if(url.indexOf('/v2/user') != -1||url.indexOf('/v1/visitors') != -1){
     //obj.result.svip_expired_at = 3742732800;
     //obj.result.is_xy_vip = true;
     //obj.result.xy_svip_expire = 3742732800;
     //obj.result.free_trial = 0;
     //obj.result.vip_type = "s";
-    obj.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY";
+    obj.result.token = Token;
+	
+}
+
+if (url.indexOf('/v3/login_by_code') != -1){
+	//{"status": "failed", "error": "Missing request parameters", "rc": -400}
+	obj = {"status":"ok","result":{"token":Token},"rc":0}
+}	
 
 $done({body:JSON.stringify(obj)})
--- js/qimao.js
+++ js/qimao.js
@@ -10,29 +10,27 @@ var body = $response.body;
 var url = $request.url;
 //var obj = JSON.parse(body);
 
-if (url.indexOf('/user/my-center') != -0x1) {
+if (url.indexOf('/user/my-center') != -1) {
     body = body.replace(/\"year_vip_show\"\:\".*?\"/g, '"year_vip_show":"1"').replace(/\"vip_show_type\"\:\".*?\"/g, '"vip_show_type":"40"').replace(/\"is_vip\"\:\".*?\"/g, '"is_vip":"1"');
-} else if (url.indexOf('/login/tourist') != -0x1) {
+} else if (url.indexOf('/login/tourist') != -1) {
     body = body.replace(/\"is_vip\"\:\".*?\"/g, '"is_vip":"1"');
-} else if (url.indexOf('/user/get-role-adv-vip-info') != -0x1) {
+} else if (url.indexOf('/user/get-role-adv-vip-info') != -1) {
     body = body.replace(/\"year_vip_show\"\:\".*?\"/g, '"year_vip_show":"1"').replace(/\"isvip\"\:\".*?\"/g, '"isvip":"1"').replace(/\"isLifetimeVip\"\:\".*?\"/g, '"isLifetimeVip":"1"').replace(/\"type\"\:\".*?\"/g, '"type":"40"');
-} else if (url.indexOf('/bookshelf-adv/index') != -0x1) {
+} else if (url.indexOf('/bookshelf-adv/index') != -1) {
     body = body.replace(/\"list\"\:\[.*?\]/g, '"list":[]');
-} else if (url.indexOf('/user/page') != -0x1) {
+} else if (url.indexOf('/user/page') != -1) {
     body = body.replace(/\"year_vip_show\"\:\".*?\"/g, '"year_vip_show":"1"').replace(/\"is_vip\"\:\".*?\"/g, '"is_vip":"1"');
-} else if (url.indexOf('/book/download') != -0x1) {
+} else if (url.indexOf('/book/download') != -1) {
     body = body.replace(/\"list\"\:\[.*?\]/g, '"list":[]');
-} else if (url.indexOf('/reader-adv/index') != -0x1) {
+} else if (url.indexOf('/reader-adv/index') != -1) {
     body = body.replace(/\"reader_chapter_list\"\:\[.*?\]/g, '"reader_chapter_list":[]').replace(/\"reader_getcoin\"\:\[.*?\]/g, '"reader_getcoin":[]').replace(/\"reader_bottom_list\"\:\[.*?\]/g, '"reader_bottom_list":[]').replace(/\"reader_page_turn_list\"\:\[.*?\]/g, '"reader_page_turn_list":[]').replace(/\"reader_noad\"\:\[.*?\]/g, '"reader_noad":[]').replace(/\"reader_inchapter\"\:\[.*?\]/g, '"reader_inchapter":[]').replace(/\"feedback\"\:\[.*?\]/g, '"feedback":[]');
-} else if (url.indexOf('/voice-adv/index') != -0x1) {
+} else if (url.indexOf('/voice-adv/index') != -1) {
     body = body.replace(/\"voice_top\"\:\[.*?\]/g, '"voice_top":[]').replace(/\"voice_bottom\"\:\[.*?\]/g, '"voice_bottom":[]');
-} else if (url.indexOf('/get-player-info') != -0x1) {
+} else if (url.indexOf('/get-player-info') != -1) {
     body = body.replace(/\"voice_list\"\:\[.*?\]/g, '"voice_list":[]');
-} else if (url.indexOf('/init-adv/index') != -0x1) {
+} else if (url.indexOf('/init-adv/index') != -1) {
     body = body.replace(/\"coopenHighList\"\:\[.*?\]/g, '"coopenHighList":[]');
-} else if (url.indexOf('/bookshelf-adv/index') != -0x1) {
+} else if (url.indexOf('/bookshelf-adv/index') != -1) {
     body = body.replace(/\"bookshelf\"\:\[.*?\]/g, '"bookshelf":[]');
 }
-$done({
-    'body': body
-});
+$done({'body': body});
