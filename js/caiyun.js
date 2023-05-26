11ada7775da976a033cbf910ab4388308aaf6ccc
DELLL-5577\huihui <wangfei021325@gmail.com>
2023-05-26 18:32:58 +0800

    no message

--- js/caiyun.js
+++ js/caiyun.js
@@ -19,21 +19,19 @@ hostname = biz.caiyunapp.com , biz.cyapi.cn ,
 
 let obj = JSON.parse($response.body)
 
-let Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY'
-
 if(url.indexOf('/v2/user') != -1||url.indexOf('/v1/visitors') != -1){
     //obj.result.svip_expired_at = 3742732800;
     //obj.result.is_xy_vip = true;
     //obj.result.xy_svip_expire = 3742732800;
     //obj.result.free_trial = 0;
     //obj.result.vip_type = "s";
-    obj.result.token = Token;
+    obj.result.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY';
 	
 }
 
 if (url.indexOf('/v3/login_by_code') != -1){
 	//{"status": "failed", "error": "Missing request parameters", "rc": -400}
-	obj = {"status":"ok","result":{"token":Token},"rc":0}
+	obj = {"status":"ok","result":{"token":'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY'},"rc":0}
 }	
 
 $done({body:JSON.stringify(obj)})
