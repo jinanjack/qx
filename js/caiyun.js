01c032d908fe1ca230465608c73d15da75cceb33
DELLL-5577\huihui <wangfei021325@gmail.com>
2023-05-26 19:02:52 +0800

    no message

--- js/caiyun.js
+++ js/caiyun.js
@@ -16,22 +16,24 @@ hostname = biz.caiyunapp.com , biz.cyapi.cn ,
 */
 
 
+var body = $response.body;
 
-let obj = JSON.parse($response.body)
-
-if(url.indexOf('/v2/user') != -1||url.indexOf('/v1/visitors') != -1){
-    //obj.result.svip_expired_at = 3742732800;
-    //obj.result.is_xy_vip = true;
-    //obj.result.xy_svip_expire = 3742732800;
-    //obj.result.free_trial = 0;
-    //obj.result.vip_type = "s";
-    obj.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY";
-	
+if(url.indexOf('/user') != -1||url.indexOf('/visitors') != -1){
+	let obj = JSON.parse(body)
+		//obj.result.svip_expired_at = 3742732800;
+		//obj.result.is_xy_vip = true;
+		//obj.result.xy_svip_expire = 3742732800;
+		//obj.result.free_trial = 0;
+		//obj.result.vip_type = "s";
+		obj.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY";
+	body = JSON.stringify(obj);
 }
 
-if (url.indexOf('/v3/login_by_code') != -1){
-	//{"status": "failed", "error": "Missing request parameters", "rc": -400}
-	obj = {"status":"ok","result":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY"},"rc":0}
+if (url.indexOf('/login_by_code') != -1){
+	let obj = JSON.parse(body)
+		//{"status": "failed", "error": "Missing request parameters", "rc": -400}
+		obj = {"status":"ok","result":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNjc0MjI3MTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.wbgfCRp3W9zEvzEYsiWxerta4G-d-b0qlYCcilevOKY"},"rc":0}
+	body = JSON.stringify(obj);
 }	
 
-$done({body:JSON.stringify(obj)})
+$done({body});
