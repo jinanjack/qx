e1850fc774df77964e8a4333c6f996753c8ecb3d
DELLL-5577\huihui <wangfei021325@gmail.com>
2023-06-25 13:11:46 +0800

    no message

--- js/Nicegram.js
+++ js/Nicegram.js
@@ -9,7 +9,7 @@ hostname = restore-access.indream.app
 ====================================
  */
 
-const a = typeof $task != 'undefined';
+/* const a = typeof $task != 'undefined';
 const b = typeof $httpClient != 'undefined';
 if (b) {
 
@@ -33,4 +33,15 @@ if (b) {
         'status': 200,
         'body': '{"data": {"premiumAccess": true}}'
     });
+} */
+
+
+const _res = {
+	status : 'HTTP/1.1 200 OK',
+	headers : {'Content-Type': 'application/json'},
+	body : {"data": {"premiumAccess": true}}
 }
+
+$done(_res);
+
+
