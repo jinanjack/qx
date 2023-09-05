9f3fde59a432e1e9fc6588dfdcee06dc0283b6f6
DELLL-5577\huihui <wangfei021325@gmail.com>
2023-09-05 14:31:02 +0800

    no message

--- js/yjb.js
+++ js/yjb.js
@@ -1,23 +1,7 @@
 /*
-
-# userinfo=/account
-# VIP=/vip_info
-# 加cang榜单=/fund_buy_ranking
-# 减cang=/fund_sell_ranking
-# 持Y=/fund_hold_ranking
-# 飙S榜=/fund_holdup_ranking
-# 切换源=/fund_gz_source
-# 会员页下部=/vip_information?page=1
-
 ====================================
 [rewrite_local]
-# 会员_万1开户
-^https?:\/\/(app-api|wx)\.yangjibao\.com\/(|wxapi\/)app_config$ url reject-dict
-# 我的_开户推广
-^https?:\/\/(app-api|wx)\.yangjibao\.com\/(|wxapi\/)scroll_list$ url reject-dict
-# 持有_顶部广告
-^https?:\/\/(app-api|wx)\.yangjibao\.com\/(|wxapi\/)unify_ad$ url reject-dict
-^https?:\/\/(app-api|wx)\.yangjibao\.com\/(|wxapi\/)(account|vip_info|fund_\w+_ranking|fund_gz_source) url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/yjb.js
+^https?:\/\/app-api\.yangjibao\.com url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/yjb.js
 
 [mitm]
 hostname = *.yangjibao.com
@@ -26,33 +10,4 @@ hostname = *.yangjibao.com
 
 var body = $response.body;
 var url = $request.url;
-
-if (url.includes('/account') || url.includes('/vip_info')) {
-    let obj = JSON.parse(body);
-    //obj.data.subscribe_status = 1;
-    obj.data.vip_label = true;
-    obj.data.is_pay = true;
-    obj.data.vip_expiry_date = '2088-08-08';
-    body = JSON.stringify(obj);
-}
-
-if (url.includes('_ranking')) {
-    let obj = JSON.parse(body);
-    obj.data.vip_label = true;
-    body = JSON.stringify(obj);
-}
-
-//会员页下部信息 [vip_info 包含 vip_information] 用reject类型APP会报网络出错
-if (url.includes('/vip_information')) {
-    let obj = JSON.parse(body);
-    obj.data.list = [];
-    body = JSON.stringify(obj);
-}
-//不可用
-if (url.includes('/fund_gz_source')) {
-    body = '{"code":200,"message":"SUCCESS","timestamp":1660536270,"data":{"source":1,"list":[1,2,3]}}';
-}
-
-$done({
-    body
-});
+$done({body});
